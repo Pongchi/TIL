16a6084fa9b826a396a1d7da033511456798454a
Pongchi <jwj1219j@naver.com>
2022-02-25 17:18:30 +0900

    Solving No.1932

--- CodingTest/BAEKJOON/1932번-정수_삼각형.py
+++ CodingTest/BAEKJOON/1932번-정수_삼각형.py
@@ -4,7 +4,15 @@ import sys
 
 n = int(sys.stdin.readline())
 arr = [ list(map(int, sys.stdin.readline().split())) for _ in range(n) ]
-memo = [0] * (n+1)
 
-for i in range(1, n+1):
-    pass
+for i in range(1, n):
+    for j in range(len(arr[i])):
+        if j == 0 or i == 1:
+            arr[i][j] += arr[i-1][0]
+        else:
+            if j != i:
+                arr[i][j] += max(arr[i-1][j-1], arr[i-1][j])
+            else:
+                arr[i][j] += arr[i-1][j-1]
+
+print( max(arr[-1]) ) 
