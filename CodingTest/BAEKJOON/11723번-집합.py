9bcbfc8f8a72ea6cc11ef8ed97153170f3db4b1c
Pongchi <jwj1219j@naver.com>
2022-03-02 17:50:46 +0900

    Update 11723번-집합.py

--- CodingTest/BAEKJOON/11723번-집합.py
+++ CodingTest/BAEKJOON/11723번-집합.py
@@ -2,29 +2,52 @@
 
 import sys
 
-SET = set()
+class SET:
+    def __init__(self):
+        self.S = set()
+
+    def add(self, x):
+        self.S.add(x)
+
+    def remove(self, x):
+        self.S.discard(x)
+
+    def toggle(self, x):
+        if x in self.S:
+            self.S.discard(x)
+        else:
+            self.S.add(x)
+    
+    def all(self):
+        self.S = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+    
+    def empty(self):
+        self.S.clear()
+
+    def check(self, x):
+        print(1 if x in self.S else 0)
+
+############################################################################################3
+
+S = SET()        
 for _ in range(int(sys.stdin.readline())):
     cmd = sys.stdin.readline().split()
-    if len(cmd) == 2:
-        cmd[1] = int(cmd[1])
-        
-    if cmd[0] == "add":
-        SET.add(cmd[1])
-    
-    elif cmd[0] == "check":
-        print(1 if cmd[1] in SET else 0)
-    
-    elif cmd[0] == "remove":
-        SET.discard(cmd[1])
-
-    elif cmd[0] == "toggle":
-        if cmd[1] in SET:
-            SET.remove(cmd[1])
+    if len(cmd) == 1:
+        if cmd[0] == "all":
+            S.all()
         else:
-            SET.add(cmd[1])
-    
-    elif cmd[0] == "all":
-        SET = set([i for i in range(1, 21)])
+            S.empty()
     
     else:
-        SET.clear()
+        cmd[1] = int(cmd[1])
+        if cmd[0] == "add":
+            S.add(cmd[1])
+        
+        elif cmd[0] == "remove":
+            S.remove(cmd[1])
+        
+        elif cmd[0] == "check":
+            S.check(cmd[1])
+
+        elif cmd[0] == "toggle":
+            S.toggle(cmd[1])    
