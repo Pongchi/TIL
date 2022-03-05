cd90cfa11f21661d89b8f41866354f3917302875
Pongchi <jwj1219j@naver.com>
2022-03-05 17:48:58 +0900

    Solving No.11727

--- CodingTest/BAEKJOON/11727번-2xn_타일링_2.py
+++ CodingTest/BAEKJOON/11727번-2xn_타일링_2.py
@@ -1,17 +1,16 @@
-# 문제 주소 : https://www.acmicpc.net/problem/11726
+# 문제 주소 : https://www.acmicpc.net/problem/11727
+
 import sys
 sys.setrecursionlimit(10**6)
 
 n = int(input())
-arr = [0] * (n+1)
+arr = [0] + [1, 3] + [0] * (n-2)
 
 def search(n):
-    if n <= 2:
-        return n
     if arr[n] != 0:
         return arr[n]
 
-    arr[n] = search(n-1) + search(n-2)
+    arr[n] = search(n-1) + (search(n-2) * 2)
     return arr[n]
 
 print( search(n) % 10007 )
