495ee46c6cde41c12d079d390f4e757294e2cc1e
Pongchi <jwj1219j@naver.com>
2022-03-09 20:02:54 +0900

    solving No.2525

--- CodingTest/BAEKJOON/2525번-오븐 시계.py
+++ CodingTest/BAEKJOON/2525번-오븐 시계.py
@@ -1,6 +1,14 @@
 # 문제 주소 : https://www.acmicpc.net/problem/2525
 
 H, M = map(int, input().split())
-C = int(input())
+C = int(input()) 
+
+M += C % 60
+if M >= 60:
+    H += 1
+M %= 60
+
+H += C // 60
+H %= 24
 
 print(H, M)
