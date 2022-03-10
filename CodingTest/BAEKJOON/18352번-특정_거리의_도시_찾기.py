417ab8236bd210b059106cd97eb39728a10141cf
Pongchi <jwj1219j@naver.com>
2022-03-10 20:22:31 +0900

    not solving No.18352
    reason : timeout

--- CodingTest/BAEKJOON/18352번-특정_거리의_도시_찾기.py
+++ CodingTest/BAEKJOON/18352번-특정_거리의_도시_찾기.py
@@ -1,16 +1,32 @@
 # 문제 주소 : https://www.acmicpc.net/problem/18352
 
 import sys
+from queue import deque
 
 N, M, K, X = map(int, sys.stdin.readline().split())
 distance = [0] * (N+1)
-graph = dict()
+graph = [[] for i in range(N+1)]
 
 for m in range(M):
-    A, B = map(int, sys.stdin.readline())
-    if A not in graph:
-        graph[A] = []
+    A, B = map(int, sys.stdin.readline().split())
     graph[A].append(B)
 
 ####################################################
 
+queue = deque([X])
+
+while queue:
+    n = queue.popleft()
+    
+    for i in graph[n]:
+        if distance[i] == 0 or distance[i] < distance[n]:
+            queue.append(i)
+            distance[i] = distance[n] + 1
+
+
+result = []
+for i in range(len(distance)):
+    if distance[i] == K:
+        result.append(i)
+
+print( "\n".join(map(str, result)) if result else -1 )
