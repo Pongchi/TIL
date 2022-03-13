b265c801ac183c680eca166a2d8029d6e85eb822
Pongchi <jwj1219j@naver.com>
2022-03-13 19:08:54 +0900

    solving No.1934
    유클리드 호제법.

--- CodingTest/BAEKJOON/1934번-최소공배수.py
+++ CodingTest/BAEKJOON/1934번-최소공배수.py
@@ -1,10 +1,7 @@
 # 문제 주소 : https://www.acmicpc.net/problem/1037
 
-import sys
+import sys, math
 
 for t in range(int(sys.stdin.readline())):
-    A, B = sorted(map(int, sys.stdin.readline().split()))
-    for i in range(1, B+1):
-        if (A*i) % B == 0:
-            print(A*i)
-            break
+    A, B = map(int, sys.stdin.readline().split())
+    print( (A*B) // math.gcd(A, B))
