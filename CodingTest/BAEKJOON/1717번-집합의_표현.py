9039172cd8731bfc339aee1cc6d88faa80ce818f
Pongchi <jwj1219j@naver.com>
2022-03-21 23:53:38 +0900

    solving No.1717
    Union by Height

--- CodingTest/BAEKJOON/1717번-집합의_표현.py
+++ CodingTest/BAEKJOON/1717번-집합의_표현.py
@@ -1,34 +1,39 @@
 import sys
 
-class disjoint_SET:
-    def __init__(self, n):
-        self.data = list(range(n+1))
-        self.size = n
-
-    def find(self, index):
-        return self.data[index]
-
-    def isInclude(self, x, y):
-        return "YES" if self.find(x) == self.find(y) else "NO"
-
-    def union(self, x, y):
-        x, y = self.find(x), self.find(y)
-
-        if x == y:
-            return
+class Set:
+    def __init__(self,N):
+        self.S = [-1 for _ in range(N+1)]
+        self.size = N+1
         
-        self.data[y] = x  
+    def find(self, i):
+        set_num = self.S[i]
+        if set_num < 0:
+            return i
+        else:
+            return self.find(set_num)
+        
+    def union(self, i, j):
+        i_root = self.find(i)
+        j_root = self.find(j)
+        if i_root != j_root:
+            if self.S[i_root] < self.S[j_root]:
+                self.S[j_root] = i_root
+            elif self.S[i_root] > self.S[j_root]:
+                self.S[i_root] = j_root
+            else:
+                self.S[i_root] -= 1
+                self.S[j_root] = i_root
+            self.size -= 1
 
-    @property
-    def length(self):
-        return len(set(self.data))
+    def isInclude(self, i, j):
+        return "YES" if self.find(i) == self.find(j) else "NO"
 
 n, m = map(int, sys.stdin.readline().split())
 
-SET = disjoint_SET(n)
+SET = Set(n)
 for operator in range(m):
     cmd, a, b = map(int, sys.stdin.readline().split())
-
+    
     if cmd == 0:
         SET.union(a, b)
     else:
