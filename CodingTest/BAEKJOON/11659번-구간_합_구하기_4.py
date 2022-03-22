f3eccaf74b4398000d54d109c5a568a473392e5d
Pongchi <jwj1219j@naver.com>
2022-03-22 22:51:59 +0900

    solving No.11659
    I did it!

--- CodingTest/BAEKJOON/11659번-구간_합_구하기_4.py
+++ CodingTest/BAEKJOON/11659번-구간_합_구하기_4.py
@@ -3,8 +3,10 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-n_lst = list(map(int, sys.stdin.readline().split()))
+arr = list(map(int, sys.stdin.readline().split()))
+for i in range(1, N):
+    arr[i] += arr[i-1]
 
 for m in range(M):
-    i, j = map(int, sys.stdin.readline().split())
-    print( sum(n_lst[i-1:j]) )
+    i, j = map(lambda x: int(x)-1, sys.stdin.readline().split())
+    print( arr[j] - arr[i-1] if i != 0 else arr[j])
