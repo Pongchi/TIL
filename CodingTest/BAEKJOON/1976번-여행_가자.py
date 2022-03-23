c2731097a28b8992bae261f832f028a3180c1e3f
Pongchi <jwj1219j@naver.com>
2022-03-24 00:25:48 +0900

    solving No.1976
    refactoring.

--- CodingTest/BAEKJOON/1976번-여행_가자.py
+++ CodingTest/BAEKJOON/1976번-여행_가자.py
@@ -2,37 +2,40 @@ import sys
 
 class Set:
     def __init__(self,N):
-        self.S = [-1 for _ in range(N+1)]
-        self.size = N
+        self.S = [-1] * N
         
     def find(self, i):
-        set_num = self.S[i]
-        if set_num < 0:
+        if self.S[i] < 0:
             return i
-        else:
-            return self.find(set_num)
+        return self.find(self.S[i])
         
     def union(self, i, j):
-        i_root = self.find(i)
-        j_root = self.find(j)
-        if i_root != j_root:
-            if self.S[i_root] < self.S[j_root]:
-                self.S[j_root] = i_root
-            elif self.S[i_root] > self.S[j_root]:
-                self.S[i_root] = j_root
-            else:
-                self.S[i_root] -= 1
-                self.S[j_root] = i_root
-            self.size -= 1
+        i = self.find(i)
+        j = self.find(j)
+
+        if i == j:
+            return
+        
+        if self.S[i] <= self.S[j]:
+            self.S[j] = i
+        else:
+            self.S[i] = j
 
 N, M = map(int, [sys.stdin.readline(), sys.stdin.readline()])
 MAP = [ list(map(int, sys.stdin.readline().split())) for _ in range(N) ]
-plan = list(map(lambda x: int(x)-1, sys.stdin.readline().split()))
-disjoint_set = Set(N)
 
+plan = []
+for p in map(lambda x: int(x)-1, sys.stdin.readline().split()):
+    if plan and plan[-1] == p:
+        clash += 1
+        continue
+    plan.append(p)
+
+disjoint_set = Set(N)
 for i in range(N):
-    for j in range(i+1, N):
+    for j in range(N):
         if MAP[i][j]:
             disjoint_set.union(i, j)
 
-print("YES" if disjoint_set.find(plan[0]) == disjoint_set.find(plan[-1]) else "NO")
+result = [ disjoint_set.find(i) for i in plan ]
+print("YES" if len(set(result)) == 1 else "NO")
