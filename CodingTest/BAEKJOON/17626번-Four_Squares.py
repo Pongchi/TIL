9bd69567df8470ffa0ff1fc3c01363f7f9083072
Pongchi <jwj1219j@naver.com>
2022-03-28 19:47:02 +0900

    solving No.17626
    But, solving by Pypy..
    
    Reason: Timeout

--- CodingTest/BAEKJOON/17626번-Four_Squares.py
+++ CodingTest/BAEKJOON/17626번-Four_Squares.py
@@ -1,6 +1,16 @@
 # 문제 주소 : https://www.acmicpc.net/problem/17626
 
 n = int(input())
+dp = [0, 1]
 
+for i in range(2, n+1):
+    _min = 50001
+    j = 1
 
-print((n ** 0.5) ** 0.5)
+    while j**2 <= i:
+        _min = min(_min, dp[i-(j**2)])
+        j += 1
+
+    dp.append(_min+1)
+
+print(dp[n])
