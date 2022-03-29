95af27cfccfb4405d049488c47969661f4576098
Pongchi <jwj1219j@naver.com>
2022-03-29 23:00:44 +0900

    Not solving No.2075
    Reason : Memory Over

---
+++ CodingTest/BAEKJOON/2075번-N번쨰 큰 수.py
@@ -0,0 +1,16 @@
+# 문제 주소 : https://www.acmicpc.net/problem/2075
+
+import heapq
+
+N = int(input())
+heap = []
+result = 0
+
+for n in range(N):
+    for i in map(int, input().split()):
+        heapq.heappush(heap, -i)
+
+for n in range(N-1):
+    heapq.heappop(heap)   
+
+print(-heapq.heappop(heap))
