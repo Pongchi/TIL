cd96067294324667a0fbf26f56385ad600eadbff
Pongchi <jwj1219j@naver.com>
2022-03-29 23:21:21 +0900

    Update 11004번-K번째 수.py
    Not solving by heap sort
    Reason : Timeout

--- CodingTest/BAEKJOON/11004번-K번째 수.py
+++ CodingTest/BAEKJOON/11004번-K번째 수.py
@@ -2,10 +2,18 @@
 
 import heapq
 
+def heap_sort(arr, k):
+    heap = []
+    for i in arr:
+        heapq.heappush(heap, i)
+
+    sorted_nums = []
+    while heap:
+        sorted_nums.append(heapq.heappop(heap))
+    
+    return sorted_nums[k-1]
+
 N, K = map(int, input().split())
 heap = list(map(int, input().split()))
 
-for n in range(K-1):
-    heapq.heappop(heap)
-
-print( heapq.heappop(heap) )
+print(heap_sort(heap, K))
