edb1efa57de76e7730b638f6a94a84df668f3000
Pongchi <jwj1219j@naver.com>
2022-03-31 23:20:02 +0900

    Update 7662번-이중_우선순위_큐.py
    Not solving.
    Reason: Timeout

--- CodingTest/BAEKJOON/7662번-이중_우선순위_큐.py
+++ CodingTest/BAEKJOON/7662번-이중_우선순위_큐.py
@@ -3,39 +3,23 @@
 import sys, heapq
 
 for T in range(int(sys.stdin.readline())):
-    print("========================")
-    max_heap = []
     min_heap = []
+    max_heap = []
     for k in range(int(sys.stdin.readline())):
         cmd, value = sys.stdin.readline().split()
         value = int(value)
 
         if cmd == "I":
-            heapq.heappush(max_heap, -value)
             heapq.heappush(min_heap, value)
-        
+            heapq.heappush(max_heap, -value)
+
         else:
-            if max_heap and min_heap:
-                tmp = []
+            if max_heap:
                 if value == 1:
                     n = heapq.heappop(max_heap)
-                    while (min_heap):
-                        tmp.append(heapq.heappop(min_heap))
-                        if tmp[-1] == n:
-                            for i in tmp[:-1]:
-                                heapq.heappush(min_heap, i)
-                            break
+                    min_heap.remove(-n)
                 else:
                     n = heapq.heappop(min_heap)
-                    while (max_heap):
-                        tmp.append(heapq.heappop(max_heap))
-                        if tmp[-1] == n:
-                            for i in tmp[:-1]:
-                                heapq.heappush(max_heap, i)
-                            break    
-  
-    if max_heap:
-        print(-heapq.heappop(max_heap), heapq.heappop(min_heap))
+                    max_heap.remove(-n)
 
-    else:
-        print("EMPTY")
+    print(" ".join(map(str, (-heapq.heappop(max_heap), heapq.heappop(min_heap)))) if max_heap else "EMPTY")
