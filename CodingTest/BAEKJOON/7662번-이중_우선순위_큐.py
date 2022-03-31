7ee68c08e4bd115610ad6804ea0061aefd1ff2a0
Pongchi <jwj1219j@naver.com>
2022-03-31 23:28:58 +0900

    Update 7662번-이중_우선순위_큐.py
    not solving
    Reason: Timeout

--- CodingTest/BAEKJOON/7662번-이중_우선순위_큐.py
+++ CodingTest/BAEKJOON/7662번-이중_우선순위_큐.py
@@ -3,23 +3,20 @@
 import sys, heapq
 
 for T in range(int(sys.stdin.readline())):
-    min_heap = []
-    max_heap = []
+    heap = []
     for k in range(int(sys.stdin.readline())):
         cmd, value = sys.stdin.readline().split()
         value = int(value)
 
         if cmd == "I":
-            heapq.heappush(min_heap, value)
-            heapq.heappush(max_heap, -value)
+            heapq.heappush(heap, value)
 
         else:
-            if max_heap:
+            if heap:
                 if value == 1:
-                    n = heapq.heappop(max_heap)
-                    min_heap.remove(-n)
+                    n = max(heap)
+                    heap.remove(n)
                 else:
-                    n = heapq.heappop(min_heap)
-                    max_heap.remove(-n)
+                    n = heapq.heappop(heap)
 
-    print(" ".join(map(str, (-heapq.heappop(max_heap), heapq.heappop(min_heap)))) if max_heap else "EMPTY")
+    print(" ".join(map(str, (max(heap), heapq.heappop(heap)))) if heap else "EMPTY")
