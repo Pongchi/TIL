347bdacd7bb499c9e14814aafc40d5919c76fa18
Pongchi <jwj1219j@naver.com>
2022-04-11 23:23:54 +0900

    Create QUADTREE.py

---
+++ CodingTest/AlgoSpot/QUADTREE.py
@@ -0,0 +1,13 @@
+# 문제 주소 : https://algospot.com/judge/problem/read/QUADTREE
+
+def ReverseQuad(compression):
+    for cmpr in compression:
+        if len(cmpr) < 4:
+            pass
+        else:
+            pass
+    return ''
+
+for C in range(int(input())):
+    cmpres = input().split('x')
+    print( ReverseQuad(cmpres) )
