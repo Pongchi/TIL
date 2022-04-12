a882573190de699a0e479dd548f80df3b349ef81
Pongchi <jwj1219j@naver.com>
2022-04-12 21:37:59 +0900

    Solving No.2156
    이전거는 반례가 있었음.
    N이 3보다 작은 경우..

--- CodingTest/BAEKJOON/2156번-포도주_시식.py
+++ CodingTest/BAEKJOON/2156번-포도주_시식.py
@@ -4,8 +4,10 @@ grape = [ int(input()) for n in range(int(input())) ]
 dp = [0] * len(grape)
 
 dp[0] = grape[0]
-dp[1] = dp[0] + grape[1]
-dp[2] = max(dp[1], dp[0]+grape[2], grape[1]+grape[2])
+if len(grape) > 1:
+    dp[1] = dp[0] + grape[1]
+    if len(grape) > 2:
+        dp[2] = max(dp[1], dp[0]+grape[2], grape[1]+grape[2])
 
 for i in range(3, len(grape)):
     dp[i] = max(dp[i-1], dp[i-2]+grape[i], dp[i-3]+grape[i]+grape[i-1])
