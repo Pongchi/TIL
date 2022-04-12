05063aecaa126ac6b0d50b0c199b01b128d67421
Pongchi <jwj1219j@naver.com>
2022-04-12 21:35:47 +0900

    solving No.2156

---
+++ CodingTest/BAEKJOON/2156번-포도주_시식.py
@@ -0,0 +1,13 @@
+# 문제 주소 : https://www.acmicpc.net/problem/2156
+
+grape = [ int(input()) for n in range(int(input())) ]
+dp = [0] * len(grape)
+
+dp[0] = grape[0]
+dp[1] = dp[0] + grape[1]
+dp[2] = max(dp[1], dp[0]+grape[2], grape[1]+grape[2])
+
+for i in range(3, len(grape)):
+    dp[i] = max(dp[i-1], dp[i-2]+grape[i], dp[i-3]+grape[i]+grape[i-1])
+
+print(dp[-1])
