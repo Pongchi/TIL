9ee2e146f6fb237a15d851f0e1f23d223d87c941
Pongchi <jwj1219j@naver.com>
2022-04-13 22:04:15 +0900

    Refactoring

--- CodingTest/BAEKJOON/11055번-가장_큰_증가_부분_수열.py
+++ CodingTest/BAEKJOON/11055번-가장_큰_증가_부분_수열.py
@@ -3,10 +3,8 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-dp = A[:]
-for i in range(1, N):
-    for j in range(i):
-        if A[i] > A[j]:
-            dp[i] = max(dp[i], dp[j]+A[i])
+dp = [0] * 1001
+for i in A:
+    dp[i] = max(dp[:i]) + i
 
 print( max(dp) )
