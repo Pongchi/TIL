40d21c926d66f1f75810fd9a313d33e75b8e5652
Pongchi <jwj1219j@naver.com>
2022-04-14 23:23:43 +0900

    solving 기능개발

--- CodingTest/PROGRAMMERS/기능개발.py
+++ CodingTest/PROGRAMMERS/기능개발.py
@@ -1,23 +1,16 @@
 def solution(progresses, speeds):
-    release_date = [ (100 - progresses[i]) // speeds[i] if ((100 - progresses[i]) / speeds[i]) % 1 == 0 else int((100 - progresses[i]) / speeds[i])+1 for i in range(len(progresses)) ]
-    today = 1
-    func = 0
     answer = []
     
-    while len(release_date) > func:
-    
-        if release_date[func] > today:
-            today += 1; continue
+    while progresses:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+        cnt = 0
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
             
-        current_func = func
-        for i in range(func, len(release_date)):
-            if release_date[i] <= today:
-                func += 1; continue
-            else:
-                answer.append(i - current_func)
-                break
-
+        if cnt > 0:
+            answer.append(cnt)
+    
     return answer
-
-print(solution([93, 30, 55], [1, 30, 5]))
-print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
