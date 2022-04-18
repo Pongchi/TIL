7ef93b0ef5f5c7ce67683225c75b76eb955e5831
Pongchi <jwj1219j@naver.com>
2022-04-18 19:27:56 +0900

    solving 가장 큰 정사각형
    와우.. 풀이를 봤지만 미쳤다 이코드는 혁신이야!!

--- CodingTest/PROGRAMMERS/가장 큰 정사각형.py
+++ CodingTest/PROGRAMMERS/가장 큰 정사각형.py
@@ -1,24 +1,8 @@
 def solution(board):
-    def isSquare(dot, s):
-        for i in range(1, s+1):
-            if not (board[dot[0]][dot[1]+i] and board[dot[0]+i][dot[1]]):
-                return False
-        return board[dot[0]+s][dot[1]+s]
-
-    max_s = 0
-    S = (len(board), len(board[0]), (lambda x, y: x if x < y else y)(len(board), len(board[0])))
-    for row in range(S[0]):
-        for column in range(S[1]):
-            if board[row][column] == 1:
-                s = 1
-                while (S[0] > row + s) and (S[1] > column + s):
-                    if not isSquare((row, column), s):
-                        break
-                    s += 1
-                    
-                if s > max_s:
-                    max_s = s
-    
-    return max_s ** 2
+    for i in range(1, len(board)):
+        for j in range(1, len(board[i])):
+            if board[i][j]:
+                board[i][j] = min(board[i-1][j-1], board[i-1][j], board[i][j-1]) + 1
+    return max( [ max(board[i]) for i in range(len(board)) ] ) ** 2
 
 print( solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]) )
