56440d3ac4e8e93d4a3a32f56b1875a7b7241dd4
Pongchi <jwj1219j@naver.com>
2022-04-20 19:02:47 +0900

    solving 카펫
    My Mistake..

--- CodingTest/STUDY/카펫.py
+++ CodingTest/STUDY/카펫.py
@@ -1,7 +1,7 @@
 # 문제 주소 : https://programmers.co.kr/learn/courses/30/lessons/42842
 
 def get_divisor(n):
-    return [ (n//i, i) for i in range(1, int(n**0.5)+1) ]
+    return [ (n//i, i) for i in range(1, int(n**0.5)+1) if n % i == 0]
 
 def solution(brown, yellow):
     for w, h in get_divisor(yellow):
