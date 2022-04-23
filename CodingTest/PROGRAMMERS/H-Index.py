1a060b225a9608fa9dda9edf7e34c4a62779b291
Pongchi <jwj1219j@naver.com>
2022-04-23 16:58:56 +0900

    solving H-Index

---
+++ CodingTest/PROGRAMMERS/H-Index.py
@@ -0,0 +1,10 @@
+# 문제주소 :
+
+def solution(citations):
+    citations.sort(reverse=True)
+    
+    for i in range(len(citations)):
+        if citations[i] < i+1:
+            return i
+        
+    return len(citations)
