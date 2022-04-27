d94581378a7f467caaa10275bc3dc4d0cf3b9530
Pongchi <jwj1219j@naver.com>
2022-04-27 22:46:06 +0900

    solving 최단 경로

--- CodingTest/BAEKJOON/1753번-최단경로.py
+++ CodingTest/BAEKJOON/1753번-최단경로.py
@@ -4,28 +4,31 @@ import sys, heapq
 
 V, E = map(int, sys.stdin.readline().split())
 START = int(sys.stdin.readline())
-graph = { node: {} for node in range(1, V+1) }
+graph = [ [] for _ in range(V+1) ]
 distances = [0] + [ float('INF') for _ in range(V) ]
-distances[START] = 0
+q = []
 
 for e in range(E):
     u, v, w = map(int, sys.stdin.readline().split())
-    graph[u][v] = w
+    graph[u].append([v, w])
 
-q = []
-heapq.heappush(q, (0, START))
+def dijkstra(start):
+    distances[start] = 0
+    heapq.heappush(q, (0, start))
 
-while q:
-    dist, now = heapq.heappop(q)
-    
-    if dist > distances[now]:
-        continue
+    while q:
+        d, now = heapq.heappop(q)
 
-    for i in graph[now]:
-        cost = dist + graph[now][i]
-        if cost < distances[i]:
-            distances[i] = cost
-            heapq.heappush(q, (cost, i))
+        if distances[now] < d:
+            continue
+        
+        for next_node, dist in graph[now]:
+            cost = d + dist
 
+            if cost < distances[next_node]:
+                distances[next_node] = cost
+                heapq.heappush(q, [cost, next_node])
+
+dijkstra(START)
 for d in distances[1:]:
     print(d if d != float('INF') else 'INF')
