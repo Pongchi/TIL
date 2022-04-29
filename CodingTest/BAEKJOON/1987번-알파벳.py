4068e185e5c3fdd63312710a3264f8cd77739b13
Pongchi <jwj1219j@naver.com>
2022-04-29 22:45:28 +0900

    solving No.1987
    Pypy3 로 제출하니까 풀림..

--- CodingTest/BAEKJOON/1987번-알파벳.py
+++ CodingTest/BAEKJOON/1987번-알파벳.py
@@ -1,7 +1,5 @@
 # 문제 주소 : https://www.acmicpc.net/problem/1987
 
-from queue import deque
-
 R, C = map(int, input().split())
 MAP = [ list(input()) for i in range(R) ]
 visited_map = [ [1]*C for i in range(R) ]
@@ -12,19 +10,21 @@ answer = 1
 
 vr = [1, -1, 0, 0]
 vc = [0, 0, 1, -1]
-queue = deque([(0, 0, 1)])
-
-while queue:
-    r, c, d = queue.popleft()
-    if d > answer:
-        answer = d
-    
-    for i in range(4):
-        nr, nc = r+vr[i], c+vc[i]
+def backtracking(distance, row, col):
+    global answer
+    if distance > answer:
+        answer = distance
         
+    for i in range(4):
+        nr, nc = row+vr[i], col+vc[i]
         if 0 <= nr < R and 0 <= nc < C and visited_map[nr][nc] and visited_alphabet[ord(MAP[nr][nc])-65]:
-            queue.append((nr, nc, d+1))
-            visited_map[nc][nc] = 0
+            visited_map[nr][nc] = 0
             visited_alphabet[ord(MAP[nr][nc])-65] = 0
 
+            backtracking(distance+1, nr, nc)
+            
+            visited_map[nr][nc] = 1
+            visited_alphabet[ord(MAP[nr][nc])-65] = 1
+
+backtracking(1, 0, 0)
 print( answer )
