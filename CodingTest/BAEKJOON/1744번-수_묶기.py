51ded2a4229ddf7a3ffb77d8a6e6b6ae48aa3b0f
Pongchi <jwj1219j@naver.com>
2022-05-02 22:28:11 +0900

    not solving No.1744
    reason : idk

--- CodingTest/BAEKJOON/1744번-수_묶기.py
+++ CodingTest/BAEKJOON/1744번-수_묶기.py
@@ -1,23 +1,28 @@
 # 문제 주소 : https://www.acmicpc.net/problem/1744
 
 N = int(input())
-arr = sorted([ int(input()) for _ in range(N) ])
-result = 0
+arr = []
+zero, result = 0, 0
+for _ in range(N):
+    n = int(input())
+    if n != 0:
+        arr.append(n)
+    else:
+        zero += 1
+
+arr.sort()
+while zero:
+    if arr[0] < 0:
+        arr.pop(0)
+    else:
+        break
 
 while len(arr) > 1:
     a, b = arr.pop(), arr.pop()
-    
-    if (a <= 0 or b <= 0) or a == b == 1:
-        if (a == 0 or b == 0) and arr and arr[-1] < 0:
-            result += a if a else b
-            arr.pop(0)
-        else:
-            if a > 0:
-                result += a + b
-            else:
-                result += a*b
-    
-    else:
+    if not (a == 1 and b == 1) and (a > 0 and b > 0) or (a < 0 and b < 0):
         result += a*b
 
-print( result + (arr[-1] if arr else 0) )
+    else:
+        result += a+b
+
+print( result + (arr[0] if arr else 0))
