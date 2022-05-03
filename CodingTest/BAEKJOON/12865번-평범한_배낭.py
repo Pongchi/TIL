b7f7c74112314726619216bc78af4904ac02854c
Pongchi <jwj1219j@naver.com>
2022-05-03 22:46:28 +0900

    refactoring No.12865

--- CodingTest/BAEKJOON/12865번-평범한_배낭.py
+++ CodingTest/BAEKJOON/12865번-평범한_배낭.py
@@ -3,15 +3,14 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
-arr = [ tuple(map(int, sys.stdin.readline().split())) for _ in range(N) ]
-dp = [ [0]*(K+1) for _ in range(N) ]
+arr = sorted([ tuple(map(int, sys.stdin.readline().split())) for _ in range(N) ])
+dp = [0]*(K+1)
 
-for i in range(N):
-    for j in range(1, K+1):
-        if (j - arr[i][0]) >= 0:
-            dp[i][j] = max( dp[i-1][j], dp[i-1][j-arr[i][0]]+arr[i][1] )
-        
-        else:
-            dp[i][j] = dp[i-1][j]
+for w, v in arr:
+    for i in range(K, -1, -1):
+        if i-w < 0:
+            break
 
-print(dp[-1][-1])
+        dp[i] = max(dp[i-w]+v, dp[i])
+
+print(dp[-1])
