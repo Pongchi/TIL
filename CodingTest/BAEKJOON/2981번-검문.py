f3cc9c5d8cad8b146a9debaa48b7581424e5dd98
Pongchi <jwj1219j@naver.com>
2022-05-06 18:20:44 +0900

    not solving 검문
    reason: timeout

--- CodingTest/BAEKJOON/2981번-검문.py
+++ CodingTest/BAEKJOON/2981번-검문.py
@@ -6,10 +6,15 @@ N = int(sys.stdin.readline())
 memo = [ int(sys.stdin.readline()) for i in range(N) ]
 
 def isValid(n):
+    tmp = memo[0] % n
+    for m in memo[1:]:
+        if tmp != m % n:
+            return False
     return True
 
-for i in range(2, min(memo)+1):
-    if isValid(n):
+result = []
+for i in range(2, max(memo)+1):
+    if isValid(i):
         result.append(i)
 
 print(" ".join(map(str, result)))
