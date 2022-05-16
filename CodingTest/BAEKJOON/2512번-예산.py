8e9016d23695687c66af3e3c8909b85d454eff30
Pongchi <jwj1219j@naver.com>
2022-05-16 21:33:11 +0900

    not solving No.2512
    reason: idk

---
+++ CodingTest/BAEKJOON/2512번-예산.py
@@ -0,0 +1,23 @@
+# 문제 주소 : https://www.acmicpc.net/problem/2512
+
+N = int(input())
+arr = list(map(int, input().split()))
+M = int(input())
+
+def sum_budget(limit):
+    s = 0
+    for i in arr:
+        s += i if i <= limit else limit
+    
+    return s
+
+start, end = 0, max(arr)
+while start <= end:
+    mid = (start + end) // 2
+
+    if sum_budget(mid) < M:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print( end )
