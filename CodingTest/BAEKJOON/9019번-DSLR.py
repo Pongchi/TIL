b5d02fba7e62023086f3ead263d642adbc6d7700
Pongchi <jwj1219j@naver.com>
2022-05-17 21:26:34 +0900

    Update 9019번-DSLR.py
    not solving.
    reason: out of memory

--- CodingTest/BAEKJOON/9019번-DSLR.py
+++ CodingTest/BAEKJOON/9019번-DSLR.py
@@ -4,31 +4,30 @@ import sys
 from queue import deque
 
 def D(n):
-    return (2 * n) % 10000
+    return str((2 * int(n)) % 10000)
 
 def S(n):
-    return n-1 if n != 0 else 9999
+    return str(int(n)-1) if int(n) != 0 else '9999'
 
 def L(n):
-    n = str(n)
-    return int(n[1:] + n[0])
+    return n[1:] + n[0]
 
 def R(n):
-    n = str(n)
-    return int(n[-1] + n[:-1])
+    return n[-1] + n[:-1]
 
 T = int(sys.stdin.readline())
 
 for _ in range(T):
-    A, B = map(int, sys.stdin.readline().split())
+    A, B = sys.stdin.readline().split()
+    B = int(B)
     queue = deque([(A, [])])
 
     while queue:
         a, log = queue.popleft()
-        if a == B:
+        if int(a) == B:
             print("".join(log))
             break
-
+        
         queue.append( (D(a), log+['D']) )
         queue.append( (S(a), log+['S']) )
         queue.append( (L(a), log+['L']) )
