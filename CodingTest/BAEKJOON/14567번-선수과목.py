85b605bdc7af9f55e759423b42eb0bcd411f35f4
Pongchi <jwj1219j@naver.com>
2022-05-24 22:40:15 +0900

    solving No.14567
    이전 코드는 틀린거였음..
    Memory | Time
    53952KB   940ms

--- CodingTest/BAEKJOON/14567번-선수과목.py
+++ CodingTest/BAEKJOON/14567번-선수과목.py
@@ -6,23 +6,17 @@ from queue import deque
 N, M = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(N) ]
 result = [1]*N
-indegree = [0] * N
 for _ in range(M):
     A, B = map(lambda x : int(x)-1, sys.stdin.readline().split())
     graph[A].append(B)
-    indegree[B] += 1
     
 queue = deque([])
 for i in range(N):
-    if indegree[i] == 0:
+    if not len(graph[i]) == 0:
         queue.append(i)
-
-    while queue:
-        now = queue.popleft()
-        for j in graph[now]:
-            indegree[j] -= 1
-            result[j] = max(result[now]+1, result[j])
-            if indegree[j] == 0:
-                queue.append(j)
+        while queue:
+            now = queue.popleft()
+            for j in graph[now]:
+                result[j] = max(result[now]+1, result[j])
 
 print(*result)
