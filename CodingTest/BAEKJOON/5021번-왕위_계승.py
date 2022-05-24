7f198e89f3ad10f69641fcc81664ee874dcfcb8b
Pongchi <jwj1219j@naver.com>
2022-05-24 22:58:19 +0900

    not solving No.5021
    혹시나 이렇게 풀어봤는데 역시나 안되네.. 가족 관계 들어오는게 바뀌면 달라서 그럼..

---
+++ CodingTest/BAEKJOON/5021번-왕위_계승.py
@@ -0,0 +1,16 @@
+# 문제 주소 : https://www.acmicpc.net/problem/5021
+
+N, M = map(int, input().split())
+KING = input()
+blood = {KING : 1.0}
+for _ in range(N):
+    child, parent1, parent2 = input().split()
+    blood[child] = (blood.get(parent1, 0.0) + blood.get(parent2, 0.0)) / 2
+
+result = input()
+for _ in range(M-1):
+    name = input()
+    if blood[result] < blood[name]:
+        result = name
+
+print( name )
