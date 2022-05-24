d89600788615e89b066a6d870d35309c007cbd72
Pongchi <jwj1219j@naver.com>
2022-05-24 22:43:09 +0900

    refactoring Code No.14567

--- CodingTest/BAEKJOON/14567번-선수과목.py
+++ CodingTest/BAEKJOON/14567번-선수과목.py
@@ -1,7 +1,6 @@
 # 문제 주소 : https://www.acmicpc.net/problem/14567
 
 import sys
-from queue import deque
 
 N, M = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(N) ]
@@ -10,13 +9,8 @@ for _ in range(M):
     A, B = map(lambda x : int(x)-1, sys.stdin.readline().split())
     graph[A].append(B)
     
-queue = deque([])
-for i in range(N):
-    if not len(graph[i]) == 0:
-        queue.append(i)
-        while queue:
-            now = queue.popleft()
-            for j in graph[now]:
-                result[j] = max(result[now]+1, result[j])
+for a in range(N):
+    for b in graph[a]:
+        result[b] = max(result[a]+1, result[b])
 
 print(*result)
