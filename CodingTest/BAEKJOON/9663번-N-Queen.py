f4d45a90a30e6d57b8ce105236a410d420fe422b
Pongchi <jwj1219j@naver.com>
2022-05-27 22:37:52 +0900

    Update 9663번-N-Queen.py
    유레카!
    계속 안됬던 이유가 fill이 0으로 다시 되돌리면서 겹치는 부분도 그냥 0으로 해버리니까 이상하게 한곳이 막 비고 그러는거여쓰 ㅡㅡ

--- CodingTest/BAEKJOON/9663번-N-Queen.py
+++ CodingTest/BAEKJOON/9663번-N-Queen.py
@@ -1,36 +1,39 @@
 # 문제 주소 : https://www.acmicpc.net/problem/9663
 
+def pprint(MAP):
+    for i in range(len(MAP)):
+        print(*MAP[i])
+
 def N_Queen(N):
     MAP = [ [0]*N for _ in range(N) ]
     result = 0
     def fill(x, y, v):
         for i in range(N):
-            MAP[x][i] = v
+            MAP[x][i] += v
         for i in range(0, N):
-            MAP[i][y] = v
-        for i in range(N):
+            MAP[i][y] += v
+        for i in range(N+1):
             if 0 <= x-i and 0 <= y-i:
-                MAP[x-i][y-i] = v
+                MAP[x-i][y-i] += v
             if 0 <= x-i and y+i < N:
-                MAP[x-i][y+i] = v
+                MAP[x-i][y+i] += v
             if x+i < N and y+i < N:
-                MAP[x+i][y+i] = v
+                MAP[x+i][y+i] += v
             if x+i < N and 0 <= y-i:
-                MAP[x+i][y-i] = v
+                MAP[x+i][y-i] += v
 
-    def recur(x):
+    def recur(x, cnt):
         nonlocal result
+        if cnt == 0:
+            result += 1
+            return
         for i in range(N):
             if MAP[x][i] == 0:
-                if x == N-1:
-                    result += 1
-                    return
-
                 fill(x, i, 1)
-                recur(x+1)
-                fill(x, i, 0)
+                recur(x+1, cnt-1)
+                fill(x, i, -1)
 
-    recur(0)
+    recur(0, N)
     return result
 
 print( N_Queen(int(input())) )
