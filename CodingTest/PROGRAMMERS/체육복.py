2e4d44bb8b549954358472ece03b92044bebda19
Pongchi <jwj1219j@naver.com>
2022-05-31 18:40:04 +0900

    refactoring 체육복
    good

--- CodingTest/PROGRAMMERS/체육복.py
+++ CodingTest/PROGRAMMERS/체육복.py
@@ -1,19 +1,12 @@
 # 문제 주소 : https://programmers.co.kr/learn/courses/30/lessons/42862
 
 def solution(n, lost, reserve):
-    overlap = [i for i in lost if i in reserve]
-    for i in overlap:
-        lost.remove(i)
-        reserve.remove(i)
-    for i in lost:
-        if i-1 in reserve: 
-            reserve.remove(i-1)
-            continue
-        elif i+1 in reserve:
-            reserve.remove(i+1)
-            continue
-        else:
-            n -= 1
-            continue
+    _reserve = list(set(reserve) - set(lost))
+    _lost = list(set(lost) - set(reserve))
+    for r in _reserve:
+        if r-1 in _lost:
+            _lost.remove(r-1)
+        elif r+1 in _lost:
+            _lost.remove(r+1)
 
-    return n
+    return n - len(_lost)
