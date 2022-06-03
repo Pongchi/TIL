8fb54f26bdbcdf30633680512ed4b0e62bcf5965
Pongchi <jwj1219j@naver.com>
2022-06-03 18:52:31 +0900

    Solving No.1049

--- CodingTest/BAEKJOON/1049번-기타줄.py
+++ CodingTest/BAEKJOON/1049번-기타줄.py
@@ -6,4 +6,6 @@ N, M = map(int, input().split())
 sorting1 = sorted([ tuple(map(int, input().split())) for _ in range(M) ], key=lambda x: x[0])
 sorting2 = sorted(sorting1, key=lambda x: x[1])
 
-print( min((N//6 * sorting1[0][0]) + (N%6 * sorting2[0][1]), math.ceil(N/6) * sorting1[0][0]) )
+print( min((N//6 * sorting1[0][0]) + (N%6 * sorting2[0][1]),
+    math.ceil(N/6) * sorting1[0][0],
+    sorting2[0][1] * N) )
