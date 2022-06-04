6d42df8041da8462966d0d16f5df28389cf67e8e
Pongchi <jwj1219j@naver.com>
2022-06-04 22:14:03 +0900

    solving 2xN 타일

--- CodingTest/PROGRAMMERS/2xn 타일.py
+++ CodingTest/PROGRAMMERS/2xn 타일.py
@@ -1,7 +1,8 @@
 # 문제 주소 : https://programmers.co.kr/learn/courses/30/lessons/12900
 
 def solution(n):
-    dp = [0, 1, 2, 3, 5]
-    while len(dp) <= n:
-        dp.append( dp[-1]+dp[-2] )
-    return dp[n]
+    a, b = 1, 2
+    for i in range(n-1):
+        a, b = b, a+b
+    
+    return a % 1000000007
