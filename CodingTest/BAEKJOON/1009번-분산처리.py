16d28cd4bca47ee20b90706635778aff40d94541
Pongchi <jwj1219j@naver.com>
2022-06-06 23:11:00 +0900

    not solving No.1009
    reason: timeout
    ..

--- CodingTest/BAEKJOON/1009번-분산처리.py
+++ CodingTest/BAEKJOON/1009번-분산처리.py
@@ -5,4 +5,9 @@ import sys
 
 for _ in range(int(sys.stdin.readline())):
     a, b = map(int, sys.stdin.readline().split())
-    print( pow(a, b) % 10 or 10)
+    result = a
+
+    for _ in range(b-1):
+        result = (result*a) % 10
+    
+    print(result or 10)
