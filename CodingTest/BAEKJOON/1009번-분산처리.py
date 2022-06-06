818a25c8caf818b8ab46b6e374b4c17e331ebf79
Pongchi <jwj1219j@naver.com>
2022-06-06 22:55:33 +0900

    not solving No.1009
    reason: timeout
    풀이법 : a**b를 줄여야 하지 않나.

---
+++ CodingTest/BAEKJOON/1009번-분산처리.py
@@ -0,0 +1,7 @@
+# 문제 주소 : https://www.acmicpc.net/problem/1009
+
+import sys
+
+for _ in range(int(sys.stdin.readline())):
+    a, b = map(int, sys.stdin.readline().split())
+    print( (a**b) % 10 or 10)
