e839ce21d6c7e8f8f67a989df7b3b2a118795206
Pongchi <jwj1219j@naver.com>
2022-06-06 23:24:20 +0900

    solving No.1009

--- CodingTest/BAEKJOON/1009번-분산처리.py
+++ CodingTest/BAEKJOON/1009번-분산처리.py
@@ -2,12 +2,15 @@
 
 import sys
 
-
+arr = [[10], [1], [6, 2, 4, 8], [1, 3, 9, 7], [6, 4], [5], [6], [1, 7, 9, 3], [6, 8, 4, 2], [1, 9]]
 for _ in range(int(sys.stdin.readline())):
     a, b = map(int, sys.stdin.readline().split())
-    result = a
 
-    for _ in range(b-1):
-        result = (result*a) % 10
+    if(a % 10) in [0, 1, 5, 6]:
+        print(arr[a%10][0])
     
-    print(result or 10)
+    elif (a % 10) in [4, 9]:
+        print(arr[a%10][b%2])
+    
+    else:
+        print(arr[a%10][b%4])
