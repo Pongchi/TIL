24f068f4a43fcd4e872f347823dba6844131d412
Pongchi <jwj1219j@naver.com>
2022-06-08 19:44:31 +0900

    not solving No.16171
    reason: idk

---
+++ CodingTest/BAEKJOON/16171번-나는_친구가_적다.py
@@ -0,0 +1,15 @@
+# 문제 주소 : https://www.acmicpc.net/problem/16171
+
+S, K = input(), input()
+
+idx = 0
+target = 0
+while idx < len(S) and target < len(K):
+    if not S[idx].isdigit():
+        if S[idx] == K[target]:
+            target += 1
+        else:
+            target = 0
+    idx += 1
+
+print( 1 if target == len(K) else 0)
