10f8bba52058f4fb11319ca80c871fc96e37a7f7
Pongchi <jwj1219j@naver.com>
2022-06-09 19:57:11 +0900

    Update 1016번-제곱ㄴㄴ수.py
    속도 빠르게..?

--- CodingTest/BAEKJOON/1016번-제곱ㄴㄴ수.py
+++ CodingTest/BAEKJOON/1016번-제곱ㄴㄴ수.py
@@ -5,9 +5,10 @@ NoNos = [False]+ [True] * (_max)
 
 for i in range(2, int(_max**0.5)+1):
     multi = i*i
-    j = 1
-    while multi*j <= _max:
-        NoNos[multi*j] = False
-        j += 1
+    if NoNos[multi]:
+        j = 1
+        while multi*j <= _max:
+            NoNos[multi*j] = False
+            j += 1
         
 print( sum(NoNos[_min:]) )
