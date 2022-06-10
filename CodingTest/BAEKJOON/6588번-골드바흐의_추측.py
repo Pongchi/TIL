681f3d9f9ec3a531e91faec617833260fc04ed07
Pongchi <jwj1219j@naver.com>
2022-06-10 22:59:36 +0900

    solving No.6588

---
+++ CodingTest/BAEKJOON/6588번-골드바흐의_추측.py
@@ -0,0 +1,26 @@
+# 문제 주소 : https://www.acmicpc.net/problem/6588
+
+import sys
+
+_max = 1000000 + 1
+primes = [True] * _max
+primes[0], primes[1] = False, False
+for i in range(2, _max):
+    if primes[i]:
+        for j in range(i*2, _max, i):
+            primes[j] = False
+
+while True:
+    n = int(sys.stdin.readline())
+    if n == 0:
+        break
+
+    if n & 1:
+        print("Goldbach's conjecture is wrong.")
+    else:
+        for i in range(2, n):
+            if primes[i] and primes[n-i]:
+                print(f"{n} = {i} + {n-i}")
+                break
+        else:
+            print("Goldbach's conjecture is wrong.")
