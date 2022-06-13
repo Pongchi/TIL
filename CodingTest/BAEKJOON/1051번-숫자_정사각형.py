2767579702d624be515634c7453816b8edc87335
Pongchi <jwj1219j@naver.com>
2022-06-13 20:31:54 +0900

    not solving No.1051
    reason: idk..
    문제를 내가 제대로 이해를 못한것 같기도 하고..

---
+++ CodingTest/BAEKJOON/1051번-숫자_정사각형.py
@@ -0,0 +1,17 @@
+# 문제 주소 : https://www.acmicpc.net/problem/1051
+
+N, M = map(int, input().split())
+MAP = [ list(map(int, input())) for _ in range(N) ]
+
+def find_max_square(n):
+    if n == 1:
+        return 1
+
+    for row in range(N-n+1):
+        for i in range(M-n+1):
+            if MAP[row][i] == MAP[row+n-1][i+n-1]:
+                return n**2
+
+    return find_max_square(n-1)
+
+print(find_max_square(N))
