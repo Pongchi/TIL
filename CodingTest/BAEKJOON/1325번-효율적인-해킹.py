4683571b2b7557a1a66c42ef45dd5c0ffdd5459d
Pongchi <67366101+Pongchi@users.noreply.github.com>
2022-06-19 19:30:11 +0900

    not solving No.1325
    reason: timeout

--- CodingTest/BAEKJOON/1325번-효율적인-해킹.py
+++ CodingTest/BAEKJOON/1325번-효율적인-해킹.py
@@ -1,13 +1,14 @@
 # 문제 주소 : https://www.acmicpc.net/problem/1325
 
 N, M = map(int, input().split())
-graph = [ [] for _ in range(N) ]
+graph = { i:[] for i in range(N) }
+trust = { i:[] for i in range(N) }
 for _ in range(M):
     A, B = map(lambda x : int(x)-1, input().split())
     graph[B].append(A)
+    trust[A].append(B)
 
-result = [ [i,0] for i in range(N) ]
-for start in range(N):
+def cnt_hacking(start):
     answer = 1
     stack = [start]
     visited = [False] * N
@@ -19,10 +20,13 @@ for start in range(N):
                 stack.append(j)
                 visited[j] = True
                 answer += 1
+    return answer
 
-    result[start][1] = answer
+result = []
+for i in trust[ max(trust.keys(), key=lambda x : len(trust[x])) ]:
+    result.append((i, cnt_hacking(i)))
 
 _max = max(result, key=lambda x : x[1])[1]
-for computer, cnt in result:
-    if cnt == _max:
-        print(computer+1, end=' ')
+for start, value in result:
+    if value == _max:
+        print(start+1, end=' ')
