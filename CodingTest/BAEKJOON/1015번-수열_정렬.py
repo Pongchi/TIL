2c840858a048072a73db16b463f224f49d1882de
Pongchi <67366101+Pongchi@users.noreply.github.com>
2022-06-24 14:02:45 +0900

    solving No.1015

--- CodingTest/BAEKJOON/1015번-수열_정렬.py
+++ CodingTest/BAEKJOON/1015번-수열_정렬.py
@@ -1,7 +1,7 @@
 # 문제 주소 : https://www.acmicpc.net/problem/1015
 
 N = int(input())
-arr = list(map(int, input().split()))
-A = sorted(enumerate(sorted(arr)), key=lambda x : x[0])
+arr = list(enumerate(map(int, input().split())))
+A = { x[0]:(i, x[1]) for i, x in enumerate(sorted(arr, key=lambda x : x[1])) }
 
-print(*[ A[i-1][0] for i in arr])
+print(*[ A[i][0] for i, v in arr ])
