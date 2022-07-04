ca8c0472d1300fd83c0492c83a8ce01043c94143
Pongchi <67366101+Pongchi@users.noreply.github.com>
2022-07-04 20:04:33 +0900

    solving No.11478
    이지 하구만.

---
+++ CodingTest/BAEKJOON/1080번-행렬.py
@@ -0,0 +1,3 @@
+# 문제 주소 : https://www.acmicpc.net/problem/1080
+
+N, M = map(int, input().split())
--- CodingTest/BAEKJOON/11478번-서로_다른_부분_문자열의_개수.py
+++ CodingTest/BAEKJOON/11478번-서로_다른_부분_문자열의_개수.py
@@ -1,3 +1,10 @@
 # 문제 주소 : https://www.acmicpc.net/problem/11478
 
-S = list(input())
+S = input()
+SET = set()
+
+for i in range(len(S)):
+    for j in range(i, len(S)):
+        SET.add( S[i:j+1] )
+
+print( len(SET) )
