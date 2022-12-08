e0f2229f2b5e2cec2eb16aed64e356fefd4f95b1
Pongchi <67366101+Pongchi@users.noreply.github.com>
2022-12-08 14:20:46 +0000

    not solving - 머리가 터진다..

--- CodingTest/BAEKJOON/14501번-퇴사.py
+++ CodingTest/BAEKJOON/14501번-퇴사.py
@@ -1,11 +1,23 @@
 # 문제 주소 : https://www.acmicpc.net/problem/14501
 
 import sys
+from collections import deque
 
 N = int(sys.stdin.readline())
 T, P = [0]*N, [0]*N
-dp = []
 for i in range(N):
     T[i], P[i] = map(int, sys.stdin.readline().split())
 
-print(N, T, P)
+dp = [0] * N
+queue = deque([ (i, P[i]) for i in range(N) if T[i]+i < N ])
+while queue:
+    days, propit = queue.popleft()
+    if days+T[days] < N:
+        dp[days] = max(propit+P[days], dp[days])
+        for i in range(N-days-T[days]):
+            if dp[days+T[days]+i] < dp[days]:
+                dp[days+T[days]+i] = dp[days]
+                queue.append((days+T[days]+i, dp[days]))
+
+print( dp[-1] )
+print(dp)
