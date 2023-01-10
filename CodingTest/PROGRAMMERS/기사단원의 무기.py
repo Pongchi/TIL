6755300cf207e86ced109af9bda896b36b406318
Pongchi <67366101+Pongchi@users.noreply.github.com>
2023-01-10 23:33:54 +0900

    Update 기사단원의 무기.py

--- CodingTest/PROGRAMMERS/기사단원의 무기.py
+++ CodingTest/PROGRAMMERS/기사단원의 무기.py
@@ -7,3 +7,16 @@
 #         return n if n <= limit else power
     
 #     return sum([ setDamage(calc_divisor(i))  for i in range(1, number+1) ])
+
+# Brute Force 지만, 위에 있는 코드보다는 빠름. 
+def solution(number, limit, power):
+    factors = [0] * (number + 1)
+    
+    for i in range(1, number+1):
+        for j in range(i, number+1, i):
+            factors[j] += 1
+            
+    return sum(map(lambda x : x if x <= limit else power, factors))
+
+
+# P.S. 에라토스테네스의 체를 사용해 약수의 개수를 찾는 방법도 있다고 한다. -> 더 빠를 것임.
